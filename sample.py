91b8be14c6a514588d9904ed4ac9b0535b75d74a
Pawel Maczuga <pmaczuga97@gmail.com>
2018-12-05 10:34:15 +0100

    sth

---
+++ sample.py
@@ -0,0 +1,4 @@
+cb = lambda x, y, z:
+	print(x)
+
+cb(3,4,5)
--- yt_downloader.py
+++ yt_downloader.py
@@ -6,14 +6,74 @@ from sys import stdout
 
 url = 'https://www.youtube.com/watch?v=f4Mc-NYPHaQ'
 
-def download_mp4(url, cb=None):
-    yt = pt.YouTube(url)
-    if cb:
-        yt.register_on_progress_callback(cb)
-    stream = yt.streams.filter(progressive=True).order_by('resolution').desc().first()
-    stream.download()
+def download(url, name=None, quality='good', on_progress=None, on_complete=None, on_error=None):
+    try:
+        yt = pt.YouTube(url)
+        if on_progress:
+            def cb(stream, chunk, file_handle, bytes_remaining):
+                on_progress(stream.filesize - bytes_remaining, stream.filesize)
+            yt.register_on_progress_callback(cb)
 
-def download_mp3(url, cb=None, cb_after_download=None, cb_convert=None):
+        streams = yt.streams.filter(progressive=True, subtype='mp4')
+        if quality == 'godd':
+            streams = streams.order_by('resolution').desc()
+        if quality == 'poor':
+            streams = streams.filter(resolution='360p')
+
+        filename = streams.first().download(filename=name)
+
+        if on_complete:
+            on_complete()
+
+        return filename
+
+    except Exception as e:
+        if on_error:
+            on_error(e)
+
+def convert(in_file, out_file, format='mp3', chunk_duration=5000, on_progress=None, on_complete=None, on_error=None):
+    try:
+        sound = AudioSegment.from_file(in_file)
+        chunk_num = sound.duration_seconds * 1000 / chunk_duration
+        try:
+            os.remove(yt.title + '.mp3')
+        except OSError:
+            pass
+        with open(in_file, 'ab') as f:
+            for i, chunk in enumerate(sound[::chunk_duration]):
+                chunk.export(f, format=format)
+                if(on_progress):
+                    on_progress(i, chunk_num)
+
+        if on_complete:
+            on_complete()
+    except Exception as e:
+        if on_error:
+            on_error(e)
+
+def download_mp4(url, on_progress=None, on_complete=None, on_error=None):
+
+    download(
+        url, 
+        quality='good',
+        on_progress=on_progress,
+        on_complete=on_complete,
+        on_error=on_error)
+
+def download_mp3(url, on_progress=None, on_half=None, on_complete=None, on_error=None):
+    download(
+        url,
+        name='tmp',
+        quality='poor',
+        on_progress=on_progress,
+        on_complete=on_complete,
+        on_error=on_error)
+
+    on_half()
+
+    convert(
+        'tmp.mp3',
+        )
     yt = pt.YouTube(url)
     if cb:
         yt.register_on_progress_callback(cb)
